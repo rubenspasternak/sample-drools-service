82326e8009c831d54aef4a0c2b78e7dd167f7b14
Rubens Pasternak <rubens.pasternak@netshoes.com>
2015-09-30 13:50:05 -0300

    Added a unit test for application context

---
+++ src/test/java/com/netshoes/inka/sample/drools/SampleDroolsApplicationTest.java
@@ -0,0 +1,17 @@
+package com.netshoes.inka.sample.drools;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SampleDroolsApplication.class)
+@WebAppConfiguration
+public class SampleDroolsApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+}
